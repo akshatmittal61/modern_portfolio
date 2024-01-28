89f24c38785178255efe43907e1dcc064311604f
Akshat Mittal <akshatmittal2506@gmail.com>
2024-01-28 19:28:35 +0530

    added external linking

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,25 +1,33 @@
-const menuBtn = document.querySelector('.menu-btn');
-const menu = document.querySelector('.menu');
-const menuNav = document.querySelector('.menu-nav');
-const menuBraanding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('.nav-item');
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBraanding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
 let showMenu = false;
-menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener("click", toggleMenu);
+
 function toggleMenu() {
-    if (!showMenu) {
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBraanding.classList.add('show');
-        navItems.forEach(item => item.classList.add('show'));
-        showMenu = true;
-    }
-    else {
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBraanding.classList.remove('show');
-        navItems.forEach(item => item.classList.remove('show'));
-        showMenu = false;
-    }
+	if (!showMenu) {
+		menuBtn.classList.add("close");
+		menu.classList.add("show");
+		menuNav.classList.add("show");
+		menuBraanding.classList.add("show");
+		navItems.forEach((item) => item.classList.add("show"));
+		showMenu = true;
+	} else {
+		menuBtn.classList.remove("close");
+		menu.classList.remove("show");
+		menuNav.classList.remove("show");
+		menuBraanding.classList.remove("show");
+		navItems.forEach((item) => item.classList.remove("show"));
+		showMenu = false;
+	}
 }
+
+document.addEventListener("keydown", function (event) {
+	if (event.key === "Escape") {
+		if (showMenu) {
+			toggleMenu();
+		}
+	}
+});
